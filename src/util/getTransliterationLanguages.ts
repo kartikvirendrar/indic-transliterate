aa5c678dc667addb1ff07e7a60b1edb4f42ffadb
kartikvirendrar <kartikrajput357@gmail.com>
2024-11-19 17:00:19 +0530

    fix build errors

---
+++ src/util/getTransliterationLanguages.ts
@@ -0,0 +1,32 @@
+import { LangObject } from "../types/LangObject";
+import { BASE_URL } from "../constants/Urls";
+
+export const getTransliterationLanguages = async (): Promise<
+  LangObject[] | undefined
+> => {
+  if (sessionStorage.getItem("indic_transliterate__supported_languages")) {
+    return JSON.parse(
+      sessionStorage.getItem("indic_transliterate__supported_languages") || "",
+    );
+  } else {
+    const apiURL = `${BASE_URL}languages`;
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const requestOptions = {
+      method: "GET",
+    };
+    try {
+      const res = await fetch(apiURL, requestOptions);
+      const data = await res.json();
+      sessionStorage.setItem(
+        "indic_transliterate__supported_languages",
+        JSON.stringify(data),
+      );
+      return data;
+    } catch (e) {
+      console.error("There was an error with transliteration", e);
+      return [];
+    }
+  }
+};
